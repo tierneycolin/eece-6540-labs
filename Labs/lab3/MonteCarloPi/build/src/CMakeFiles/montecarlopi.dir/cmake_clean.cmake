28f96383c1e9538c1919a29e16cef0f6ec862d3b
tierneycolin <tierneyco@merrimack.edu>
2023-06-18 11:30:00 -0700

    CPU and GPU execution complete

---
+++ Labs/lab3/MonteCarloPi/build/src/CMakeFiles/montecarlopi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/montecarlopi.dir/monte_carlo_pi.cpp.o"
+  "montecarlopi"
+  "montecarlopi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/montecarlopi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
